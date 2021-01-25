5583dfcdb42a4d94da4fbda763164ebd09823e84
David Florentin <43811108+davidflorentin@users.noreply.github.com>
2021-01-25 19:37:18 +0000

    Add files via upload
    2020 update

--- app.R
+++ app.R
@@ -16,39 +16,46 @@ mortCause <- read.csv("mortCause.csv")
 #user interface
 ui <- fluidPage(includeCSS("viz.css"),
 
-  tags$header(h1("How we die: England & Wales"),
+  tags$header(h1("How people die: England & Wales"),
   #tags$p("In England & Wales"),
   fluidRow(column(3,
   #set up cohort select input
-  selectInput(inputId = "cohort", label = "Birth period (age in 2015)",
-              c("1916-1920 (95-100)" =  1920,
-                "1921-1925 (90-94)" = 1925,
-                "1926-1930 (85-89)" = 1930,
-                "1931-1935 (80-84)" = 1935,
-                "1935-1940 (75-79)" = 1940,
-                "1941-1945 (70-74)" = 1945,
-                "1946-1950 (65-69)" = 1950,
-                "1951-1955 (60-64)" = 1955,
-                "1956-1960 (55-59)" = 1960,
-                "1961-1965 (50-54)" = 1965,
-                "1966-1970 (45-49)" = 1970,
-                "1971-1975 (40-44)" = 1975,
-                "1976-1980 (35-39)" = 1980,
-                "1981-1985 (30-34)" = 1985,
-                "1986-1990 (25-29)" = 1990,
-                "1991-1995 (20-24)" = 1995,
-                "1996-2000 (15-19)" = 2000,
-                "2001-2005 (10-14)" = 2005,
-                "2006-2010 (5-9)" = 2010),
-              selected = 1970)),
+  selectInput(inputId = "cohort", label = "Birth period (age in 2019)",
+              c("1935-1939 (80-84)" = 1939,
+                "1940-1944 (75-79)" = 1944,
+                "1945-1949 (70-74)" = 1949,
+                "1950-1954 (65-69)" = 1954,
+                "1955-1959 (60-64)" = 1959,
+                "1960-1964 (55-59)" = 1964,
+                "1965-1969 (50-54)" = 1969,
+                "1970-1974 (45-49)" = 1974,
+                "1975-1979 (40-44)" = 1989,
+                "1980-1984 (35-39)" = 1984,
+                "1985-1989 (30-34)" = 1989,
+                "1990-1994 (25-29)" = 1994,
+                "1995-1999 (20-24)" = 1999,
+                "2000-2004 (15-19)" = 2004,
+                "2005-2009 (10-14)" = 2009,
+                "2010-2014 (5-9)" = 2014),
+              selected = 1969)),
   column(6,
   #set up radio buttons for sex selection
   radioButtons(inputId = "sex", label = "Sex", inline = TRUE, choices = c("Male" = "1", "Female" = "2"))))),
   tags$div(class = "spacer"), #prevents elements loading behind fixed header
   
+  tags$div(
+    tags$p("This dashboard takes the number of births in a 5-year period and progressively subtracts registered deaths from that cohort as they age.
+           Births are based on ONS population data for the ", tags$a(href = "https://webarchive.nationalarchives.gov.uk/20160111174808/http://www.ons.gov.uk/ons/publications/re-reference-tables.html?edition=tcm%3A77-215593",
+           "20th Century"), " and ", tags$a(href = "https://www.ons.gov.uk/peoplepopulationandcommunity/populationandmigration/populationestimates/datasets/the21stcenturymortalityfilespopulationdataset",
+           "21st Century. "),"Deaths are based on the ONS mortality files for the ", tags$a(href = "https://webarchive.nationalarchives.gov.uk/20160108034247tf_/http://www.ons.gov.uk/ons/rel/subnational-health1/the-20th-century-mortality-files/20th-century-deaths/index.html",
+           "20th Century"), " and ", tags$a(href = "https://www.ons.gov.uk/peoplepopulationandcommunity/birthsdeathsandmarriages/deaths/datasets/the21stcenturymortalityfilesdeathsdataset",
+           "21st Century.")),
+    tags$hr()),
+    
   #plot graphs
   tags$div(
   plotlyOutput(outputId = "timeseries", height = 350),
+  tags$p("The bar graph shows the top 4 causes of death (and 'Other') for each age group. Click on a bar to see the full breakdown below."),
   plotlyOutput(outputId = "stackedBar", height = 350)),
   
   tags$div(
@@ -57,7 +64,6 @@ ui <- fluidPage(includeCSS("viz.css"),
   #footer information
   tags$hr(),
   tags$div(
-  tags$p("*'% alive' is calculated based on births minus registered deaths and does not account for migration."),
   tags$p("Cause of death is based on ICD version in use at time of death"),
   tags$p("Data sources:"),
   tags$a(href = "https://www.ons.gov.uk/peoplepopulationandcommunity/birthsdeathsandmarriages/deaths/datasets/the21stcenturymortalityfilesdeathsdataset",
@@ -78,7 +84,7 @@ server <- function(input, output) {
   #first graph of time series
   output$timeseries <- renderPlotly({
     subset(popDeath, U_yearBorn == input$cohort & sex == input$sex)-> selectedCohort
-    
+   
     plot_ly(width = 800, height = 300) %>% 
       layout(xaxis = list(title = "Last year of period", fixedrange = TRUE),
              yaxis = list(title = "% alive*", fixedrange=TRUE),
@@ -109,14 +115,14 @@ server <- function(input, output) {
         paste("% deaths:", pctOfDeaths),
         sep ="\n"),
         yearLabel = paste(paste(as.numeric(U_year) - 4, U_year, sep="-"),
-                          paste(U_year-as.numeric(input$cohort), U_year-as.numeric(input$cohort)+4, sep = "-"), sep="\n")) -> mainCauses
+                          paste(U_year-as.numeric(input$cohort), U_year-as.numeric(input$cohort)+4, sep = "-"), sep="/")) -> mainCauses
     
       #plot stacked bar chart of main causes
     mainCauses %>% 
       plot_ly(source = "source", x = ~yearLabel, y=~pctOfDeaths, type ="bar", color=~cause, width = 800, height =320,
               text = c(mainCauses$hovertext), hoverinfo ='text') %>% 
       layout(barmode = "stack", xaxis = list(title = "Year/Age", fixedrange=TRUE), yaxis = list(title = "% of deaths", fixedrange=TRUE),
-             title = paste("Cause of deaths", if_else(as.numeric(input$cohort)-4 > 1971, as.numeric(input$cohort)-4, 1971),"-2015:",
+             title = paste("Cause of deaths", if_else(as.numeric(input$cohort)-4 > 1970, as.numeric(input$cohort)-4, 1970),"-2019:",
                            if_else(input$sex == 1, "Males", "Females"),
                            "born", as.numeric(input$cohort)-4, "to", input$cohort)) %>%
       config(displayModeBar = F)
@@ -128,9 +134,9 @@ server <- function(input, output) {
       #get event_data so it updates when the stack bar chart is hovered over
       eventdata <- event_data("plotly_click", source = "source")
       
-      validate(need(!is.null(eventdata) & eventdata$x[1] > 1970, "Select a time period to show detailed causes of death"))
+      validate(need(!is.null(eventdata) & eventdata$x[1] > 1965, "Select a column on the bar chart to show detailed causes of death"))
       
-      selectedYear <- if_else(is.null(substr(eventdata$x[1],6,9)), "2015", substr(eventdata$x[1],6,9))
+      selectedYear <- if_else(is.null(substr(eventdata$x[1],6,9)), "2019", substr(eventdata$x[1],6,9))
       
       #filter mortality cause data by selected cohort and sex information
       mortCause %>% filter(U_yearBorn == input$cohort & sex == input$sex & U_year == selectedYear) -> causeDeath
